a8cbad353e7a3365527c827f5d92bd5f2a20601e
David Delgado <dadch13.14@gmail.com>
2020-06-07 19:29:17 -0400

    Autenticación con Google usando OpenID Connect.

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -18,6 +18,9 @@ require('./utils/auth/strategies/basic');
 // OAuth Strategy
 require('./utils/auth/strategies/oauth');
 
+// Google Strategy
+require('./utils/auth/strategies/google');
+
 const THIRTY_DAYS_IN_SEC = 2592000000;
 const TWO_HOURS_IN_SEC = 7200000;
 
@@ -125,6 +128,32 @@ app.get(
   passport.authenticate('google-oauth', {
     session: false,
   }),
+  function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile', 'openid'],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { session: false }),
   function (req, res, next) {
     if (!req.user) {
       next(boom.unauthorized());
